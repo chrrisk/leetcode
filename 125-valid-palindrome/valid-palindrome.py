41589d7ed8bc43525eab5d424a06e1bbf4d886a8
chrrisk <129128127+chrrisk@users.noreply.github.com>
2026-07-09 01:06:20 -0400

    Time: 7 ms (81.08%) | Memory: 19.5 MB (93.97%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        start = 0
+        end = len(s) - 1
+
+        while start < end:
+            if s[start].isalnum() == False:
+                start += 1
+                continue
+            if s[end].isalnum() == False:
+                end -= 1
+                continue
+            if s[start].lower() == s[end].lower():
+                start += 1
+                end -= 1
+                continue
+            if s[start].lower() != s[end].lower():
+                return False
+        return True
