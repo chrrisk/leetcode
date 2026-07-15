112ce3aab91692eca373fd4a09706584d6884f00
chrrisk <129128127+chrrisk@users.noreply.github.com>
2026-07-14 22:08:01 -0400

    Time: 74 ms (8.99%) | Memory: 98 MB (5.06%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const countSeen = new Map();
+    for (let i = 0; i < nums.length; i++){
+        if (countSeen.has(`${nums[i]}`)){
+            // countSeen.set(`${nums[i]}`,countSeen.get(`${nums[i]}`));
+            return true;
+        } else{
+            countSeen.set(`${nums[i]}`,1);
+        }
+    }
+    return false;
+};
