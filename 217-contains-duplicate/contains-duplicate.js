ac779ceca7fdd77d1fda0073194019cdead5da1f
chrrisk <129128127+chrrisk@users.noreply.github.com>
2026-07-17 16:30:04 -0400

    Time: 26 ms (35.29%) | Memory: 79.7 MB (16.87%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -2,17 +2,25 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+// var containsDuplicate = function(nums) {
+//     const counts = new Map();
+//     for (let n of nums) {
+//         if (counts.has(n)) {
+//             counts.set(n, counts.get(n) + 1);
+//         } else {
+//             counts.set(n, 1);
+//         }
+//     }
+//     for (const [num, count] of counts) {
+//         if (count > 1) return true;
+//     }
+//     return false;
+// };
 var containsDuplicate = function(nums) {
-    const counts = new Map();
+    const seen = new Set();
     for (let n of nums) {
-        if (counts.has(n)) {
-            counts.set(n, counts.get(n) + 1);
-        } else {
-            counts.set(n, 1);
-        }
-    }
-    for (const [num, count] of counts) {
-        if (count > 1) return true;
+        if (seen.has(n)) return true;
+        seen.add(n);
     }
     return false;
 };
